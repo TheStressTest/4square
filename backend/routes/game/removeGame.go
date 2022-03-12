935246bbfe5c041217603e5081c29d05d5bc5cb5
Funnylimericks <73505616+TheStressTest@users.noreply.github.com>
2022-03-12 14:07:20 -0800

    Implement remove game

--- backend/routes/game/removeGame.go
+++ backend/routes/game/removeGame.go
@@ -1,1 +1,24 @@
 package game
+
+import (
+	"backend/database"
+	"backend/models"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RemoveGame(ctx *fiber.Ctx) error {
+	i, err := strconv.Atoi(ctx.Get("id"))
+
+	if err != nil {
+		ctx.JSON(map[string]string{"error": err.Error()})
+		return ctx.SendStatus(400)
+	}
+
+	if err := database.DB.Delete(&models.Game{}, i).Error; err != nil {
+		ctx.JSON(map[string]string{"error": err.Error()})
+		return ctx.SendStatus(400)
+	}
+	return ctx.SendStatus(200)
+}
